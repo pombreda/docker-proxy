7622d88e3a6d81c90cf612ad491b2c0ee1081ca8
Meaglith Ma <genedna@gmail.com>
2014-01-17 03:03:53 -0500

    Add check for *.docker.io

--- dockerproxy.go
+++ dockerproxy.go
@@ -2,12 +2,21 @@ package main
 
 import (
   "github.com/elazarl/goproxy"
-  "log"
   "net/http"
+  "regexp"
 )
 
 func main() {
   proxy := goproxy.NewProxyHttpServer()
+  proxy.OnRequest().DoFunc(
+    func(r *http.Request, ctx *goproxy.ProxyCtx) (*http.Request, *http.Response) {
+      match, _ := regexp.MatchString("^*.docker.io$", r.URL.Host)
+      if match == false {
+        return r, goproxy.NewResponse(r, goproxy.ContentTypeText, http.StatusForbidden, "Don't use it for other proxy!")
+      } else {
+        return r, nil
+      }
+    })
   proxy.Verbose = true
-  log.Fatal(http.ListenAndServe(":8384", proxy))
+  http.ListenAndServe(":8384", proxy)
 }
