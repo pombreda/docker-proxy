c797c4855182e8be295bc6cbbb2510932a87308e
Meaglith Ma <genedna@gmail.com>
2014-01-18 12:23:44 +0800

    Change the abuse warning words.

--- dockerproxy.go
+++ dockerproxy.go
@@ -12,7 +12,8 @@ func main() {
     func(r *http.Request, ctx *goproxy.ProxyCtx) (*http.Request, *http.Response) {
       match, _ := regexp.MatchString("^*.docker.io$", r.URL.Host)
       if match == false {
-        return r, goproxy.NewResponse(r, goproxy.ContentTypeText, http.StatusForbidden, "Don't use it for other proxy!")
+        return r, goproxy.NewResponse(r, goproxy.ContentTypeText, http.StatusForbidden,
+          "The proxy is used exclusively to download docker image, please don't abuse it for any purpose.")
       } else {
         return r, nil
       }
